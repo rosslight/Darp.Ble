f377e38a280d68521a118538d0e2cefa69d69159
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-01-22 16:56:36 +0100

    fix: Smaller comments

--- src/Darp.Ble.Mock/IMockBleBroadcaster.cs
+++ src/Darp.Ble.Mock/IMockBleBroadcaster.cs
@@ -4,18 +4,12 @@ using Darp.Ble.Implementation;
 
 namespace Darp.Ble.Mock;
 
-/// <summary>
-/// The mock ble broadcaster
-/// </summary>
+/// <summary> The mock ble broadcaster </summary>
 public interface IMockBleBroadcaster : IBleBroadcaster
 {
-    /// <summary>
-    /// The definition of delegate for OnGetAdvertisements
-    /// </summary>
+    /// <summary> The definition of delegate for OnGetAdvertisements </summary>
     public delegate IObservable<IGapAdvertisement> GetAdvertisements(BleObserver observer, AdvertisingParameters? parameters, CancellationTokenSource? cancellationTokenSource);
 
-    /// <summary>
-    /// The delegate for OnGetAdvertisements
-    /// </summary>
+    /// <summary> The delegate for OnGetAdvertisements </summary>
     public GetAdvertisements? OnGetAdvertisements { get; set; }
 }
