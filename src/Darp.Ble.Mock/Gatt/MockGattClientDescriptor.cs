42de33968ddb45555a47598ac8ba2a40dbd37165
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-03-10 15:10:03 +0100

    chore: Clean up unused descriptor classes

--- src/Darp.Ble.Mock/Gatt/MockGattClientDescriptor.cs
+++
@@ -1,8 +0,0 @@
-using Darp.Ble.Gatt.Client;
-
-namespace Darp.Ble.Mock.Gatt;
-
-internal sealed class MockGattClientDescriptor(
-    MockGattClientCharacteristic clientCharacteristic,
-    IGattCharacteristicValue value
-) : GattClientDescriptor(clientCharacteristic, value);
