ae59d7b9564906a24ffd34138e59cc8977de872e
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-07-08 17:35:13 +0200

    feat: Multi targeting Darp.Ble.WinRT
    - Behave as Noop
    - Raise a warning

--- src/Darp.Ble.WinRT/WinBleFactory.cs
+++ src/Darp.Ble.WinRT/WinBleFactory.cs
@@ -1,7 +1,16 @@
+using System.Runtime.Versioning;
+#if !WINDOWS
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+#endif
+
 namespace Darp.Ble.WinRT;
 
 /// <summary> Search for the default windows ble device </summary>
-public sealed class WinBleFactory : IBleFactory
+/// <remarks> If used in an environment which is not build for windows, this becomes a NoOp </remarks>
+[SupportedOSPlatform("windows")]
+public sealed partial class WinBleFactory : IBleFactory
 {
     /// <summary> The name of the resulting device </summary>
     public string Name { get; set; } = "Windows";
@@ -9,6 +18,34 @@ public sealed class WinBleFactory : IBleFactory
     /// <inheritdoc />
     public IEnumerable<IBleDevice> EnumerateDevices(IServiceProvider serviceProvider)
     {
+#if WINDOWS
         yield return new WinBleDevice(serviceProvider, Name);
+#else
+        var logger = serviceProvider.GetService<ILogger<WinBleFactory>>();
+        if (logger is not null)
+            LogNoWindowsUnintentionalUse(logger, nameof(WinBleFactory), GetTargetFrameworkMoniker());
+        yield break;
+#endif
     }
+
+#if !WINDOWS
+    [LoggerMessage(
+        Level = LogLevel.Warning,
+        Message = "Using the {FactoryName} while not compiled for Windows is probably unintended! You compiled for {TargetFrameworkName}"
+    )]
+    private static partial void LogNoWindowsUnintentionalUse(
+        ILogger logger,
+        string factoryName,
+        string? targetFrameworkName
+    );
+
+    private static string? GetTargetFrameworkMoniker()
+    {
+        Assembly assembly = typeof(WinBleFactory).Assembly;
+        AssemblyMetadataAttribute? meta = assembly
+            .GetCustomAttributes<AssemblyMetadataAttribute>()
+            .FirstOrDefault(a => a.Key.Equals("TargetFramework", StringComparison.Ordinal));
+        return meta?.Value;
+    }
+#endif
 }
