2104915ead61ce5e85ebcf2fa2c82d1e08e67e33
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-10-30 22:00:07 +0100

    fix: Fix a number of bugs with the H4TransportLayer
    - Making sure everything is async
    - Reading ACL headers in the correct size
    - Proper cleanup behavior

--- src/Darp.Ble.Hci/Transport/ITransportLayer.cs
+++ src/Darp.Ble.Hci/Transport/ITransportLayer.cs
@@ -3,7 +3,7 @@ using Darp.Ble.Hci.Package;
 namespace Darp.Ble.Hci.Transport;
 
 /// <summary> An abstract implementation of a transport layer </summary>
-public interface ITransportLayer : IDisposable
+public interface ITransportLayer : IAsyncDisposable
 {
     /// <summary> Enqueue a new hci packet </summary>
     /// <param name="packet"> The packet to be enqueued </param>
